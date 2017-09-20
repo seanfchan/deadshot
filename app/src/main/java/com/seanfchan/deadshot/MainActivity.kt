6445ea500dbbbb4bc715eef4dd0c77b704ed184b
Sean F Chan <seanchan@twitter.com>
2017-09-20 04:48:22 -0700

    moved creation of the sensor streams into a util class

--- app/src/main/java/com/seanfchan/deadshot/MainActivity.kt
+++ app/src/main/java/com/seanfchan/deadshot/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.seanfchan.deadshot
 
-import android.Manifest
 import android.content.Context
-import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener2
 import android.hardware.SensorManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Toast
@@ -23,7 +16,6 @@ import io.reactivex.ObservableEmitter
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,7 +36,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        setUpCompassObservable()
+        setUpRotationObservable()
         setUpLocationObservable()
     }
 
@@ -54,40 +46,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setUpLocationObservable() {
-        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val locationObservable = Observable.create({ emitter: ObservableEmitter<Location> ->
-            val listener = object : LocationListener {
-                override fun onLocationChanged(p0: Location?) {
-                    if (p0 != null) {
-                        emitter.onNext(p0)
-                    }
-                }
-
-                override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
-                }
-
-                override fun onProviderEnabled(p0: String?) {
-                }
-
-                override fun onProviderDisabled(p0: String?) {
-                }
-            }
-
-            emitter.setCancellable {
-                locationManager.removeUpdates(listener)
-                emitter.onComplete()
-            }
-
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
-                    PackageManager.PERMISSION_GRANTED) {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, TimeUnit.SECONDS.toMillis(5), 0f, listener)
-            } else {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION_CODE )
-                emitter.onComplete()
-            }
-        })
-
-        compositeDisposable.add(locationObservable.subscribeOn(AndroidSchedulers.mainThread())
+        compositeDisposable.add(Util.getGPSSensorStream(this, ACCESS_FINE_LOCATION_CODE)
+                .subscribeOn(AndroidSchedulers.mainThread())
                 .observeOn(Schedulers.computation())
                 .doOnNext({
                     it.altitude
@@ -97,37 +57,8 @@ class MainActivity : AppCompatActivity() {
                 .subscribe())
     }
 
-    fun setUpCompassObservable() {
-        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        val sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
-        if (sensor == null) {
-            Toast.makeText(this, "You do not have the sensors to create a compass", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        val compassEventObservable = Observable.create<SensorEvent> {
-            emitter: ObservableEmitter<SensorEvent> ->
-            val listener = object : SensorEventListener2 {
-                override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-                }
-
-                override fun onFlushCompleted(p0: Sensor?) {
-                }
-
-                override fun onSensorChanged(event: SensorEvent) {
-                    emitter.onNext(event)
-                }
-            }
-
-            emitter.setCancellable({
-                sensorManager.unregisterListener(listener, sensor)
-                emitter.onComplete()
-            })
-
-            sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
-        }
-
-        compositeDisposable.add(compassEventObservable
+    fun setUpRotationObservable() {
+        compositeDisposable.add(Util.getRotationVectorObservable(this)
                 .doOnNext {
                     val rotMat = FloatArray(9)
                     val azimuthAboutYAxis = FloatArray(9)
--- app/src/main/java/com/seanfchan/deadshot/util/Util.kt
+++ app/src/main/java/com/seanfchan/deadshot/util/Util.kt
@@ -1,9 +1,94 @@
 package com.seanfchan.deadshot.util
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener2
+import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
+import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+import java.util.concurrent.TimeUnit
 
 object Util {
     fun isPermissionGranted(grantResults: IntArray): Boolean {
-        return grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        return grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun getGPSSensorStream(activity: Activity, requestPermissionCode: Int): Observable<Location> {
+        val appContext = activity.applicationContext
+        val locationManager = appContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return Observable.create({ emitter: ObservableEmitter<Location> ->
+            val listener = object : LocationListener {
+                override fun onLocationChanged(p0: Location?) {
+                    if (p0 != null) {
+                        emitter.onNext(p0)
+                    }
+                }
+
+                override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+                }
+
+                override fun onProviderEnabled(p0: String?) {
+                }
+
+                override fun onProviderDisabled(p0: String?) {
+                }
+            }
+
+            emitter.setCancellable {
+                locationManager.removeUpdates(listener)
+                emitter.onComplete()
+            }
+
+            if (ContextCompat.checkSelfPermission(appContext, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, TimeUnit.SECONDS.toMillis(5), 0f, listener)
+            } else {
+                ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), requestPermissionCode)
+                emitter.onComplete()
+            }
+        })
+    }
+
+    fun getRotationVectorObservable(activity: Activity): Observable<SensorEvent> {
+        val appContext = activity.applicationContext
+        val sensorManager = appContext.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+        if (sensor == null) {
+            Toast.makeText(activity, "You do not have the sensors to create a compass", Toast.LENGTH_SHORT).show()
+            return Observable.error(Error("Do not have the correct sensors for a rotation vector fusion sensor"))
+        }
+
+        return Observable.create<SensorEvent> {
+            emitter: ObservableEmitter<SensorEvent> ->
+            val listener = object : SensorEventListener2 {
+                override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+                }
+
+                override fun onFlushCompleted(p0: Sensor?) {
+                }
+
+                override fun onSensorChanged(event: SensorEvent) {
+                    emitter.onNext(event)
+                }
+            }
+
+            emitter.setCancellable({
+                sensorManager.unregisterListener(listener, sensor)
+                emitter.onComplete()
+            })
+
+            sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        }
     }
 }
